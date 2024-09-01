62715f8fc8df26bf39f76b0bd519a12c416cb9b1
mowahid latif <mowahidlatif@hotmail.com>
2024-09-01 12:02:34 -0500

    User Data setup, needs testing

---
+++ controllers/usersController.js
@@ -0,0 +1,34 @@
+const User = require("../models/User");
+
+// Create a new user
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+// Get all users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      success: true,
+      data: users,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
